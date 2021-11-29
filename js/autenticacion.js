388ad3b39c01152f52e93302b41d94eb0c6a8fca
ecologaroom <ecologaroom@gmail.com>
2021-11-29 15:20:45 -0600

    Autenticación prueba 1

--- js/autenticacion.js
+++ js/autenticacion.js
@@ -1,4 +1,4 @@
-/*  Initializa Firebase con la configuración del proyecto. */
+/*  Inicializa Firebase con la configuración del proyecto. Revisa la configuración en tu servidor de Firebase y cópiala aquí sustituyendo los asteriscos. Los campos deben quedar igual que en tu servidor. */
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyAXoqlSzgA2an2L6uSSOtXFX4Q4zHr0Mvc",
--- js/login.js
+++ js/login.js
@@ -1,40 +1,46 @@
-// Login con Google
-function formLogin(){
-    /* Conexión al sistema de autenticación de Firebase. */
+docum
+
+const nombre = get
+
+/* Conexión al sistema de autenticación de Firebase. */
     // @ts-ignore
     const auth = firebase.auth();
     /* Tipo de autenticación de usuarios. En este caso es con Google. */
     // @ts-ignore
     const provider = new firebase.auth.GoogleAuthProvider();
+
+// Login con Google
+function formLogin(){
     /* Configura el proveedor de Google para que permita seleccionar de una lista. */
     provider.setCustomParameters({ prompt: "select_account" });
     /* Recibe una función que se invoca cada que hay un cambio en la autenticación y recibe el modelo con las características del usuario. */
     auth.onAuthStateChanged(
         /* Recibe las características del usuario o null si no ha iniciado sesión. */
-        async usuarioAuth => {
+        usuarioAuth => {
             if (usuarioAuth && usuarioAuth.email) {
-                /* Nombre de usuario atenticado por Firebase y aceptado.
-                let usuario = usuarioAuth.email; */
-                // Muestra los mensajes del chat.
-                muestraMensajes();
+                /* Nombre de usuario atenticado por Firebase y aceptado. */
+                nombre.value = usuarioAuth.email; 
             } else {
                 /* No ha iniciado sesión. Pide datos para iniciar sesión. */
-                await auth.signInWithRedirect(provider);
+                auth.signInWithRedirect(provider);
             }
         },
         /* Función que se invoca si hay un error al verificar el usuario. */
-        procesaError()
+        procesaError
     );
 };
+
+/* Terminar la sesión. */
+async function terminaSesion() {
+    try {
+        await auth.singOut();
+    } catch (e) {
+        procesaError(e);
+    }
+}
 
 /* Procesa un error. Muestra el objeto en la consola y un cuadro de alerta con el mensaje. @param {Error} e descripción del error. */
 function procesaError(e) {
   console.log(e);
   alert(e.message);
 }
-
-/* Muestra los mensaje almacenados en la collection "MENSAJE". Se actualiza automáticamente. */
-function muestraMensajes() {
-  location.href = 'FAQ.html';
-}
-  
