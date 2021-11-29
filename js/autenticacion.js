b517f1e337a5754d41813f9f18b940b5bba569fb
NekoSakura023cv1 <dashix2010@gmail.com>
2021-11-29 14:11:51 -0600

    Cambio de cuenta del proyecto

---
+++ js/autenticacion.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAXoqlSzgA2an2L6uSSOtXFX4Q4zHr0Mvc",
+  authDomain: "ecologaroom.firebaseapp.com",
+  projectId: "ecologaroom",
+  storageBucket: "ecologaroom.appspot.com",
+  messagingSenderId: "509252941570",
+  appId: "1:509252941570:web:e651c919f5d0902470741b",
+  measurementId: "G-45R2RM4HV8"
+});
---
+++ js/navegacion.js
@@ -0,0 +1,23 @@
+function formInicio(){
+    location.href = 'index.html';
+};
+
+function formHabitaciones(){
+    location.href = 'habitaciones.html';
+};
+
+function formInstalaciones(){
+    location.href = 'instalaciones.html';
+};
+
+function formTurismo(){
+    location.href = 'turismo.html';
+};
+
+function formRestaurantes(){
+    location.href = 'restaurantes.html';
+};
+
+function formFAQ(){
+    location.href = 'FAQ.html';
+};
