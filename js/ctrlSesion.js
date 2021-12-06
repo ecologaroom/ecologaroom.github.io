560d5d18cf258b2afa5bd537141288257f9f1f9a
ecologaroom <ecologaroom@gmail.com>
2021-12-06 00:39:21 -0600

    login

--- js/ctrlSesion.js
+++ js/ctrlSesion.js
@@ -7,9 +7,6 @@ import {
     muestraError
 } from "../lib/util.js";
 
-
-const reservacion = document.getElementById("reservar");
-
 /* Conexión con Firestore. */
 const firestore = getFirestore();
 
@@ -25,7 +22,8 @@ function enviaSesion(usuario) {
         // Usuario aceptado.
         rol();
     } else {
-        // No ha iniciado sesión.                    logIn(); ////////////////////////////////////////7
+        // No ha iniciado sesión.
+        logIn();
     }
 }
 
@@ -56,7 +54,7 @@ async function tieneRol(usuario, roles) {
         location.href = "index.html";
     } else {
         /* Si se tiene rol verdadero se manda al Login */
-        ////////////////////////////////////////////////////////////////////////////////////logIn();
+        logIn();
     }
     return false;
 }
@@ -76,31 +74,14 @@ async function cargaRoles(email) {
 }
 
 /* Ejecuta login con Google */
-function logIn() {
+async function logIn() {
     /* Tipo de autenticación de usuarios. En este caso es con Google. */
     // @ts-ignore
     const provider = new firebase.auth.GoogleAuthProvider();
     /* Configura el proveedor de Google para que permita seleccionar de una lista. */
     provider.setCustomParameters({ prompt: "select_account" });
-    /* Recibe una función que se invoca cada que hay un cambio en la autenticación y recibe el modelo con las características del usuario.*/
-  
-    getAuth().onAuthStateChanged(
-        /* Recibe las características del usuario o null si no ha iniciado sesión. */
-        async usuarioAuth => {
-            if (usuarioAuth && usuarioAuth.email) {
-                // Usuario aceptado y con login
-                location.href = 'reservacion_cliente.html';
-            } else {
-                // No ha iniciado sesión. Pide datos para iniciar sesión.
-                await getAuth().signInWithRedirect(provider)
-            }
-        },
-        // Función que se invoca si hay un error al verificar el usuario.
-        muestraError
-    )
-
-    /* Pide datos para iniciar sesión. 
-    await getAuth().signInWithRedirect(provider);*/
+    /* Pide datos para iniciar sesión. */
+    await getAuth().signInWithRedirect(provider);
 }
 
 /* Cierra sesión */
