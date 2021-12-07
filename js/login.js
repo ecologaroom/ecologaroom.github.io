f546216b3337b4bfc1939a52b0bd443a311c0e0b
ecologaroom <ecologaroom@gmail.com>
2021-12-07 13:28:43 -0600

    login

--- js/login.js
+++ js/login.js
@@ -4,11 +4,15 @@ import {
 
 /* Ejecuta login con Google */
 async function logIn() {
+
+    // @ts-ignore
+    const auth = firebase.auth();
+
     /* Tipo de autenticación de usuarios. En este caso es con Google. */
     // @ts-ignore
     const provider = new firebase.auth.GoogleAuthProvider();
     /* Configura el proveedor de Google para que permita seleccionar de una lista. */
     provider.setCustomParameters({ prompt: "select_account" });
     /* Pide datos para iniciar sesión. */
-    await getAuth().signInWithRedirect(provider);
+    await auth.signInWithRedirect(provider);
 }
