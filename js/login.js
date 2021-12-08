a9645fb24b671e04a66a983dbcd737153000df24
ecologaroom <ecologaroom@gmail.com>
2021-12-07 20:03:06 -0600

    login

---
+++ js/login.js
@@ -0,0 +1,49 @@
+function formLogin(){
+    /* Conexión al sistema de autenticación de Firebase. */
+    // @ts-ignore
+    const auth = firebase.auth();
+    /* Tipo de autenticación de usuarios. En este caso es con Google. */
+    // @ts-ignore
+    const provider = new firebase.auth.GoogleAuthProvider();
+  
+    /* Configura el proveedor de Google para que permita seleccionar de una lista. */
+    provider.setCustomParameters({ prompt: "select_account" });
+    /* Recibe una función que se invoca cada que hay un cambio en la autenticación y recibe el modelo con las características del usuario.*/
+    auth.onAuthStateChanged(
+      /* Recibe las características del usuario o null si no ha iniciado sesión. */
+      async usuarioAuth => {
+        if (usuarioAuth && usuarioAuth.email) {
+          // Usuario aceptado y con login
+          location.href = 'reservacion_cliente.html';
+        } else {
+          // No ha iniciado sesión. Pide datos para iniciar sesión.
+          await auth.signInWithRedirect(provider)
+          /*provider.getRedirectResult().then(function(result) {
+            if (usuarioAuth) {
+              location.href = 'reservacion_cliente.html'; //After successful login, user will be redirected to home.html
+            }
+          })*/
+        }
+      },
+      // Función que se invoca si hay un error al verificar el usuario.
+      procesaError
+    )      
+  }
+  
+  /* Terminar la sesión. */
+  async function terminaSesion() {
+    /* Conexión al sistema de autenticación de Firebase. */
+    // @ts-ignore
+    const auth = firebase.auth();
+    try {
+        await auth.singOut();
+    } catch (e) {
+        procesaError(e);
+    }
+  }
+  
+  /* Procesa un error. Muestra el objeto en la consola y un cuadro de alerta con el mensaje. @param {Error} e descripción del error. */
+  function procesaError(e) {
+    console.log(e);
+    alert(e.message);
+  } 
