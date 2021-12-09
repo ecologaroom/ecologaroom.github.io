34e428d51aa7914c673f5fd07ce951a2cab12141
ecologaroom <ecologaroom@gmail.com>
2021-12-09 09:54:39 -0600

    Arreglo de registro de datos del cliente

--- js/ctrlClienteNuevo.js
+++ js/ctrlClienteNuevo.js
@@ -1,134 +1,152 @@
 /* Conexión al sistema de Firestore. */
 // @ts-ignore
 const firestore = firebase.firestore();
-const daoCliente = firestore.collection("Cliente");
+const daoCliente = firestore.collection("CLIENTE");
 // @ts-ignore
-const daoUsuario = firestore.collection("Usuario");
-
-/** @type {HTMLFormElement} */
-const formUsuario = document["formUsuario"];
+const daoUsuario = firestore.collection("USUARIO");
 
 /* Conexión al sistema de autenticación de Firebase. */
-  // @ts-ignore
+// @ts-ignore
 const auth = firebase.auth();
-auth.onAuthStateChanged(tieneRol, procesaError);
+auth.onAuthStateChanged(protege, procesaError);
 
-/** Valida que sea cliente */
-async function validaCliente(usuario) {
-  alert("Validando que sea cliente");
-  if (tieneRol(usuario,["Cliente"])) {
-    registroCliente
-  } 
- 
+/** @type {HTMLFormElement} */
+const formDaoCliente = document["formDaoCliente"];
+/** @type {HTMLFormElement} */
+const formDaoReservacion = document["formDaoReservacion"];
+/** @type {HTMLFormElement} */
+const formDaoPago = document["formDaoPago"];
+
+/** Valida que sea cliente
+ * @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  alert("Entra a protege");
+  if (tieneRol(usuario,["CLIENTE"])) {
+    alert("Es cliente");
+    formDaoCliente.addEventListener("submit", registroCliente);
+    alert("Ya agregó accion de registro a cliente");
+    formDaoReservacion.addEventListener("submit", realizaReservacion);
+    alert("Ya agregó accion de reservar");
+    formDaoPago.addEventListener("submit", efectuaPago);
+    alert("Ya agregó accion de pagar");
+  }
 }
 
-async function registroCliente(){
-  alert("Si es un cliente");
-  try {
-    alert("Entra al try");
-    const formData = new FormData(formUsuario);
-    const nombre = getString(formData, "nombre").trim();
-    alert("nombre" + nombre);  
-    const ap_paterno = getString(formData, "ap_paterno").trim();
-    alert("ap_materno" + ap_paterno); 
-    const ap_materno = getString(formData, "ap_materno").trim();
-    alert("ap_materno" + ap_materno); 
-    const edad = getString(formData, "edad").trim();
-    alert("edad" + edad);
-    const sexo = getString(formData, "sexo").trim();
-    alert("sexo" + sexo); 
-    const celular = getString(formData, "celular").trim();
-    alert("celular" + celular); 
-    const correo = getString(formData, "correo").trim();
-    alert("correo" + correo); 
+/** Revisa si la sesión está iniciada y comprueba si es el rol autorizado.
+ * @param {import(
+    "../lib/tiposFire.js").User}
+    usuario
+ * @param {string[]} roles
+ * @returns {Promise<boolean>} */
+async function tieneRol(usuario, roles) {
+  alert("Verifica inicio de sesión");
+  if (usuario && usuario.email) {
+    alert("Si inició sesión");
+    const rolIds = await cargaRoles(usuario.email);
+    alert("Rol cargado");
+    for (const rol of roles) {
+      if (rolIds.has(rol)) {
+        alert("Rol autorizado");
+        return true;
+      }
+    }
+    alert("No autorizado.");
+    location.href = "index.html";
+  } else {
+    alert("No inició sesión");
+    logIn();
+  }
+  return false;
+}
+
+/** Busca en la colección de Usuario si existe rol para el email del usuario.
+ * @param {string} email
+ * @returns {Promise<Set<string>>}
+ */
+ async function cargaRoles(email) { 
+  let doc = await firestore.collection("USUARIO").doc(email).get();
+  alert("Obtuvo rol de la colección");
+  if (doc.exists) {
     /**
-     ** @type { import("./tipos.js").Alumno} */
-    const modelo = {nombre, ap_paterno, ap_materno, edad, sexo, celular, correo};
+     * @type {
+        import("./tipos.js").
+        USUARIO} */
+    const data = doc.data();
+    alert("Si tiene rol asignado");
+    return new Set(data.ROLIDS || []);
+  } else {
+    alert("No tiene rol asignado");
+    return new Set();
+  }
+}
+
+async function logIn() {
+  alert("Rediccionando a google");
+  /** Autenticación con Google.
+   * @type {import(
+      "../lib/tiposFire.js").
+      GoogleAuthProvider} */
+  const provider =
+    // @ts-ignore
+    new firebase.auth.GoogleAuthProvider();
+  /* Configura el proveedor de Google para que permita seleccionar de una lista. */
+  provider.setCustomParameters({ prompt: "select_account" });
+  await auth.signInWithRedirect(provider);
+}
+
+
+
+
+
+
+
+
+async function registroCliente(){
+  try {
+    alert("Entra al try de registroCliente");
+    const formDataCliente = new FormData(formDaoCliente);
+
+    const NOMBRE = getString(formDataCliente, "nombre").trim();
+    alert("nombre" + NOMBRE);  
+    const AP_PATERNO = getString(formDataCliente, "ap_paterno").trim();
+    alert("ap_materno" + AP_PATERNO); 
+    const AP_MATERNO = getString(formDataCliente, "ap_materno").trim();
+    alert("ap_materno" + AP_MATERNO); 
+    const EDAD = getString(formDataCliente, "edad").trim();
+    alert("edad" + EDAD);
+    const SEXO = getString(formDataCliente, "sexo").trim();
+    alert("sexo" + SEXO); 
+    const CELULAR = getString(formDataCliente, "celular").trim();
+    alert("celular" + CELULAR); 
+    const CORREO = getString(formDataCliente, "correo").trim();
+    alert("correo" + CORREO); 
+    
+    /**
+     * @type {
+        import("./tipos.js").
+                CLIENTE} */
+    const modelo = {CORREO, NOMBRE, AP_PATERNO, AP_MATERNO, EDAD, SEXO, CELULAR};
     alert("Guarda datos en modelo");
-    await daoCliente.add(modelo); 
+
+    await firestore.collection("CLIENTE").add(modelo); 
     alert("Sus datos han sido registrados exitosamente.");
   } catch (e) {
     procesaError(e);
   }
 }
 
-/** @param {import(
-    "../lib/tiposFire.js").User}
-    usuario
- * @param {string[]} roles
- * @returns {Promise<boolean>} */
-async function tieneRol(usuario, roles) {
-  alert("Validando inicio de sesión");
-  if (usuario && usuario.email) {
-    alert("Si tiene sesión iniciada");
-    const rolIds = await cargaRoles(usuario.email);
-
-    for (const rol of roles) {
-      alert("For de roles");
-      if (rolIds.has(rol)) {
-        alert("Su rol coincide con el de un cliente");
-        return true;
-      } else {
-        alert("No autorizado.");
-        location.href = "index.html";
-      }
-    }
-  } else {
-    alert("Lo manda a logear");
-    logIn();
-  }
-  return false;
-}
-
-/** Busca si existe un rol y lo toma 
- * @param {string} email
- * @returns {Promise<Set<string>>}
- */
-async function cargaRoles(email) { 
-    alert("Buscando su rol");
-    /* Busa en la colección Usuario el email con el que se autesentificó */
-    let doc = await daoUsuario.doc(email).get();
-    if (doc.exists) {
-      /**
-       * @type {
-          import("./tipos.js").
-          USUARIO} */
-      alert("Existe rol así que lo retorna");
-      const data = doc.data();
-      /* Existe email con rol, así que lo manda */
-      return new Set(data.ROLIDS || []);
-    } else {
-        /* No existe email con rol, así que devuleve vacío */
-      alert("No existe email con rol, así que devuleve vacío");
-      return new Set();
-    }
-}
-
-async function logIn() {
-  /* Tipo de autenticación de usuarios. En este caso es con Google. */
-// @ts-ignore
-  const provider = new firebase.auth.GoogleAuthProvider();
-  /* Configura el proveedor de Google para que permita seleccionar de una lista. */
-  provider.setCustomParameters({ prompt: "select_account" });
-  /* Recibe una función que se invoca cada que hay un cambio en la autenticación y recibe el modelo con las características del usuario.*/
-  await auth.signInWithRedirect(provider);
-}
 
 
-/**
- * @param {FormData} formData
- * @param {string} name */
-function getString(formData, name) {
-  alert("Convirtiendo a String");
-  const valor = formData.get(name);
-  return (typeof valor === "string" ? valor : "" );
-}
 
-/* Procesa un error. Muestra el objeto en la consola y un cuadro de alerta con el mensaje. @param {Error} e descripción del error. */
-function procesaError(e) {
-  console.log(e);
-  alert(e.message);
-} 
+
+
+
+
+
+
+
 
 /** @type {HTMLFormElement} */
 const formReservacion = document["formReservacion"];
@@ -209,11 +227,60 @@ async function efectuaPago(usuario) {
   }
 } 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/** Procesa un error. Muestra el objeto en la consola y un cuadro de alerta con el mensaje. @param {Error} e descripción del error. 
+ * @param {Error} e */
+ function procesaError(e) {
+  console.log(e);
+  alert(e.message);
+}
+
+/**
+ * @param {FormData} formData
+ * @param {string} name */
+function getString(formData, name) {
+  alert("Convirtiendo a String");
+  const valor = formData.get(name);
+  return (typeof valor === "string" ? valor : "" );
+}
+
+/** Evita la inyección de código de textos que puedan interpretarse como HTML.
+ * @param {string} texto
+ * @returns {string} un texto que
+ *  no puede interpretarse como
+ *  HTML. */
+function escape(texto) {
+  return (texto || "").toString().replace(/[<>"']/g, reemplaza);
+}
+
+/** Reemplaza caracteres especiales.
+ * @param {string} letra */
+function reemplaza(letra) {
+  switch (letra) {
+      case "<": return "&lt;";
+      case ">": return "&gt;";
+      case '"': return "&quot;";
+      case "'": return "&#039;";
+      default: return letra;
+  }
+}
+
 /* Terminar la sesión. */
 async function logOut() {
   try {
     /* Conecta a Firebase para cerrar sesión */
-    await auth.signOut();
+    await auth.signOut(location.href = "index.html");
   } catch (e) {
     procesaError(e);
   }
