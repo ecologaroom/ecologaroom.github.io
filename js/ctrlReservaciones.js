c6496cec4bc7abf801c6268771b05352ef98f829
ecologaroom <ecologaroom@gmail.com>
2021-12-09 22:59:51 -0600

    Búsqueda por clv huesped

--- js/ctrlReservaciones.js
+++ js/ctrlReservaciones.js
@@ -197,7 +197,7 @@ function reservaHuesped() {
   tab.innerHTML += '<tr><th colspan="1">#Habitación</th><th colspan="1">Reservación</th><th colspan="1">Estatus</th><th colspan="1">Huésped</th><th colspan="1">Fecha de reservación</th><th colspan="1">Entrada</th><th colspan="1">Salida</th><th colspan="1">#Huéspedes</th></tr>'
 
   /* Registros de la colección Reservación, ordenados por número de habitación */
-  firestore.collection("RESERVACION").where("FECHA_ENTRADA", "==", fechaLlegada).get().then(function(snap){
+  firestore.collection("RESERVACION").where("CLV_HUESPED", "==", nom).get().then(function(snap){
     alert("Entra al where");
     if (snap.size > 0) {
       /* Cuando el número de documentos es 0, agrega un texto HTML. */
