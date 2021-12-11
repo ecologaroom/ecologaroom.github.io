dd44aec8bdac5cdb1e9df3c40e5307448dbcb955
ecologaroom <ecologaroom@gmail.com>
2021-12-11 00:35:53 -0600

    hoy

--- js/ctrlReservaciones.js
+++ js/ctrlReservaciones.js
@@ -76,9 +76,12 @@ function consulta() {
   tab.innerHTML = "";
   tab.innerHTML += '<tr><th colspan="1">#Habitación</th><th colspan="1">Reservación</th><th colspan="1">Estatus</th><th colspan="1">Huésped</th><th colspan="1">Fecha de reservación</th><th colspan="1">Entrada</th><th colspan="1">Salida</th><th colspan="1">#Huéspedes</th></tr>'
 
+  var hoy = Date.now();
+  alert(hoy);
+
   /* Registros de la colección Reservación, ordenados por número de habitación */
   // @ts-ignore
-  firestore.collection("RESERVACION").orderBy("NUM_HABITACION").get().then(function(snap){
+  firestore.collection("RESERVACION").where("FECHA_ENTRADA", ">=", hoy).orderBy("NUM_HABITACION").get().then(function(snap){
     if (snap.size > 0) {
       /* Cuando el número de documentos es 0, agrega un texto HTML. */
       snap.forEach(function(doc){
@@ -165,13 +168,11 @@ function reservaFecha() {
   var calendario = document.getElementById("calendario").value;
   var fecha = calendario.toString().split("-").reverse().join("-");
 
-  alert("Fecha calendario ="+fecha);
-
   if(fecha == "" || fecha == "dd-mm-aaaa"){
     consulta();
   } else {
     /* Registros de la colección Reservación, ordenados por número de habitación */
-    firestore.collection("RESERVACION").where("FECHA_ENTRADA", "==", fecha).get().then(function(snap){
+    firestore.collection("RESERVACION").where("FECHA_ENTRADA", "==", fecha).orderBy("NUM_HABITACION").get().then(function(snap){
       if (snap.size > 0) {
         /* Cuando el número de documentos es 0, agrega un texto HTML. */
         snap.forEach(function(doc){
